36044f75a470d2e5c704e96e481d6577b02383ec
prashant523580 <prashant.thapa62758@gmail.com>
2023-03-07 01:45:48 +0545

    type assign

---
+++ src/types/product.ts
@@ -0,0 +1,9 @@
+export type ProductTypes ={
+    title?: string,
+    price?: string,
+    description?:string,
+    category?:string,
+    image?: string,
+    rating?: any
+    qty?:any
+}
---
+++ src/types/user.ts
@@ -0,0 +1,14 @@
+export interface UserTypes {
+    email?: string,
+    password?: string,
+    name?: string,
+    phone?: string
+}
+export interface UserAddressTypes {
+    name?: string,
+    email?: string,
+    phone?: string,
+    city? :string,
+    street?: string,
+
+}
