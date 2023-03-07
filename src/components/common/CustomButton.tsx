b0b9bf221e65e9c87cac14417f0db2019a6d8e79
prashant523580 <prashant.thapa62758@gmail.com>
2023-03-08 01:25:05 +0545

    added 'borderWidth','borderColor', 'icon', 'icon Color

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,30 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 interface PropsTypes{
     title: string,
-    onPress: () => void
-    fgColor:string
-    bgColor:string
+    onPress?: () => void
+    fgColor?:string
+    bgColor?:string
     width?:any,
-    my?:number
+    my?:number,
+    borderWidth?:any,
+    borderColor?:any,
+    style?:any,
+    icon?:any,
+    iconColor?:any
 }
 export default function CustomButton(props: PropsTypes) {
   return (
-    <TouchableOpacity style={{
+    <TouchableOpacity style={[{
         backgroundColor:props.bgColor,
         borderRadius:8,
-        elevation: 2,
+        // elevation: 1,
         paddingHorizontal: 10,
         paddingVertical:10,
         width:props.width,
-        marginVertical:props.my
-    }} onPress={props.onPress}>
+        marginVertical:props.my,
+        borderColor:props.borderColor,
+        borderWidth:props.borderWidth
+    },props.style]} onPress={props.onPress}>
+      {
+          props.icon ? <Image source={props.icon} style={{
+            width:25, height:25,tintColor:props.iconColor
+          }}/>
+
+          :
         <Text style={{
-            color:props.fgColor,
-            textAlign:"center",
-            width:"100%"
+          color:props.fgColor,
+          textAlign:"center",
+          width:"100%"
         }}>{props.title}</Text>
+      }
     </TouchableOpacity>
   )
 }
